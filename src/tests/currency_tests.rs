e7b582ecd65f6d4b62e944c26ed85334005248cb
murmaider227 <osakihor@gmail.com>
2023-04-04 20:19:18 +0300

    test: add test_price_command_invalid_currency

--- src/tests/currency_tests.rs
+++ src/tests/currency_tests.rs
@@ -1,17 +1,18 @@
 use crate::commands::price::price_command;
-// use super::*;
+use dotenvy::dotenv;
+//use super::*;
 
-// #[tokio::test]
-// async fn test_price_command_invalid_currency() {
-//     dotenv().ok();
-//     let value = 1.0;
-//     let currency = "CURRENCY".to_string();
-//     let result = price_command(value, currency).await;
-//     assert_eq!(
-//         result,
-//         "Error fetching price for CURRENCY: Currency not found"
-//     );
-// }
+#[tokio::test]
+async fn test_price_command_invalid_currency() {
+    dotenv().ok();
+    let value = 1.0;
+    let currency = "CURRENCY".to_string();
+    let result = price_command(value, currency).await;
+    assert_eq!(
+        result,
+        "Error fetching price for CURRENCY: Currency not found"
+    );
+}
 
 #[tokio::test]
 async fn test_price_command_invalid_value() {
